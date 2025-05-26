0e2a619078131947d460a9b754381dc9e77b4e21
arition <aritionkb@gmail.com>
2025-05-26 00:50:47 -0700

    update deps

--- Jellyfin.Plugin.Ignore.Test/IgnoreTest.cs
+++ Jellyfin.Plugin.Ignore.Test/IgnoreTest.cs
@@ -25,5 +25,15 @@ namespace Jellyfin.Plugin.Ignore.Test
             Assert.True(!ignore.IsIgnored("cd/sps/thebestshot.mkv"));
             Assert.True(!ignore.IsIgnored("/cd/sps/thebestshot.mkv"));
         }
+
+        [Fact]
+        public void TestReal()
+        {
+            var ignore = new global::Ignore.Ignore();
+            ignore.Add("\\[VCB-Studio\\] Sword Art Online -Ordinal Scale- \\[Ma10p_2160p_SDR\\]\\[x265_flac\\].mkv");
+
+            Assert.True(ignore.IsIgnored("/_done/movie/[VCB-Studio] Sword Art Online -Ordinal Scale- [Ma10p_2160p_SDR][x265_flac].mkv"));
+            Assert.True(!ignore.IsIgnored("/_done/movie/[VCB-Studio] Sword Art Online -Ordinal Scale- [Ma10p_2160p_DoVi_P8.1][x265_flac].mkv"));
+        }
     }
 }
